a805ebd1750980ede93db76a5244d801fb23b8ec
Jason Ainsworth and Sean Pierce <jason@>
2017-07-05 14:20:10 -0700

    add user auth and styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
-  # resources :users
+  # routes for user auth
+  resources :users, :except => [:edit, :update]
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
