54605f44b7c9a22cbc11f27a99dfbc723e6ab2ef
Jason Ainsworth and Sean Pierce <sean@>
2017-07-05 14:28:01 -0700

    complete user auth 🆒

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
 
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
   end
 
+  def authorize
+    if !current_user
+      flash[:alert] = "You aren't authorized to visit that page."
+      redirect_to '/'
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
 
   # before_action :authenticate_user!
 
-  
+  attr_accessor :password
+  validates_confirmation_of :password
+  validates :email, :user_name, :presence => true, :uniqueness => true
+  before_save :encrypt_password
+
+  def encrypt_password
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password_hash = BCrypt::Engine.hash_secret(password,password_salt)
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by "email = ?", email
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
 
 end
